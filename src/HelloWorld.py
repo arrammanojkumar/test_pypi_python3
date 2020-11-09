e30c47beef438b0290b3d80c12b94296088063bb
Manoj Kumar <marram@innominds.com>
2020-11-09 16:56:08 +0530

    added src

---
+++ src/HelloWorld.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+__author__ = "Manoj Kumar Arram"
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,3 @@
 #!/bin/python3
 
 __author__ = "Manoj Kumar Arram"
-
-print('Hello World')
